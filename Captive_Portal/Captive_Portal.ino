651bd46205e93b020e8ef325dfad506a13f79cbc
Yash Sanghvi <50194122+sanghvi-yash@users.noreply.github.com>
2021-03-28 12:53:00 +0530

    Removed ESP8266 dependency in Captive Portal

--- Captive_Portal/Captive_Portal.ino
+++ Captive_Portal/Captive_Portal.ino
@@ -1,11 +1,6 @@
 #include <DNSServer.h>
-#ifdef ESP32
 #include <WiFi.h>
 #include <AsyncTCP.h>
-#elif defined(ESP8266)
-#include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
-#endif
 #include "ESPAsyncWebServer.h"
 
 DNSServer dnsServer;
