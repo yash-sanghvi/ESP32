9ef1bedbef5e7c8173d0c47bb35646f771058065
Yash Sanghvi <50194122+sanghvi-yash@users.noreply.github.com>
2021-08-25 00:05:27 +0530

    Added example to Open Captive Portal only if WiFi Connection fails in setup

---
+++ Open_CaptivePortal_When_WiFiConnect_Fails/Open_CaptivePortal_When_WiFiConnect_Fails.ino
@@ -0,0 +1,179 @@
+#include <DNSServer.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include "ESPAsyncWebServer.h"
+#include <Preferences.h>
+
+Preferences preferences;
+DNSServer dnsServer;
+AsyncWebServer server(80);
+
+String ssid;
+String password;
+bool is_setup_done = false;
+bool valid_ssid_received = false;
+bool valid_password_received = false;
+bool wifi_timeout = false;
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html><head>
+  <title>Captive Portal Demo</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head><body>
+  <h3>Captive Portal Demo</h3>
+  <br><br>
+  <form action="/get">
+    <br>
+    SSID: <input type="text" name="ssid">
+    <br>
+    Password: <input type="text" name="password">
+    <input type="submit" value="Submit">
+  </form>
+</body></html>)rawliteral";
+
+class CaptiveRequestHandler : public AsyncWebHandler {
+  public:
+    CaptiveRequestHandler() {}
+    virtual ~CaptiveRequestHandler() {}
+
+    bool canHandle(AsyncWebServerRequest *request) {
+      //request->addInterestingHeader("ANY");
+      return true;
+    }
+
+    void handleRequest(AsyncWebServerRequest *request) {
+      request->send_P(200, "text/html", index_html);
+    }
+};
+
+void setupServer() {
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send_P(200, "text/html", index_html);
+    Serial.println("Client Connected");
+  });
+
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest * request) {
+    String inputMessage;
+    String inputParam;
+
+    if (request->hasParam("ssid")) {
+      inputMessage = request->getParam("ssid")->value();
+      inputParam = "ssid";
+      ssid = inputMessage;
+      Serial.println(inputMessage);
+      valid_ssid_received = true;
+    }
+
+    if (request->hasParam("password")) {
+      inputMessage = request->getParam("password")->value();
+      inputParam = "password";
+      password = inputMessage;
+      Serial.println(inputMessage);
+      valid_password_received = true;
+    }
+    request->send(200, "text/html", "The values entered by you have been successfully sent to the device. It will now attempt WiFi connection");
+  });
+}
+
+
+void WiFiSoftAPSetup()
+{
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("esp-captive");
+  Serial.print("AP IP address: "); Serial.println(WiFi.softAPIP());
+}
+
+
+void WiFiStationSetup(String rec_ssid, String rec_password)
+{
+  wifi_timeout = false;
+  WiFi.mode(WIFI_STA);
+  char ssid_arr[20];
+  char password_arr[20];
+  rec_ssid.toCharArray(ssid_arr, rec_ssid.length() + 1);
+  rec_password.toCharArray(password_arr, rec_password.length() + 1);
+  Serial.print("Received SSID: "); Serial.println(ssid_arr); Serial.print("And password: "); Serial.println(password_arr);
+  WiFi.begin(ssid_arr, password_arr);
+
+  uint32_t t1 = millis();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(2000);
+    Serial.print(".");
+    if (millis() - t1 > 50000) //50 seconds elapsed connecting to WiFi
+    {
+      Serial.println();
+      Serial.println("Timeout connecting to WiFi. The SSID and Password seem incorrect.");
+      valid_ssid_received = false;
+      valid_password_received = false;
+      is_setup_done = false;
+      preferences.putBool("is_setup_done", is_setup_done);
+
+      StartCaptivePortal();
+      wifi_timeout = true;
+      break;
+    }
+  }
+  if (!wifi_timeout)
+  {
+    is_setup_done = true;
+    Serial.println("");  Serial.print("WiFi connected to: "); Serial.println(rec_ssid);
+    Serial.print("IP address: ");  Serial.println(WiFi.localIP());
+    preferences.putBool("is_setup_done", is_setup_done);
+    preferences.putString("rec_ssid", ssid);
+    preferences.putString("rec_password", password);
+  }
+}
+
+void StartCaptivePortal() {
+  Serial.println("Setting up AP Mode");
+  WiFiSoftAPSetup();
+  Serial.println("Setting up Async WebServer");
+  setupServer();
+  Serial.println("Starting DNS Server");
+  dnsServer.start(53, "*", WiFi.softAPIP());
+  server.addHandler(new CaptiveRequestHandler()).setFilter(ON_AP_FILTER);//only when requested from AP
+  server.begin();
+  dnsServer.processNextRequest();
+}
+
+void setup() {
+  //your other setup stuff...
+  Serial.begin(115200);
+  Serial.println();
+  preferences.begin("my-pref", false);
+
+  is_setup_done = preferences.getBool("is_setup_done", false);
+  ssid = preferences.getString("rec_ssid", "Sample_SSID");
+  password = preferences.getString("rec_password", "abcdefgh");
+
+  if (!is_setup_done)
+  {
+    StartCaptivePortal();
+  }
+  else
+  {
+    Serial.println("Using saved SSID and Password to attempt WiFi Connection!");
+    Serial.print("Saved SSID is ");Serial.println(ssid);
+    Serial.print("Saved Password is ");Serial.println(password);
+    WiFiStationSetup(ssid, password);
+  }
+
+  while (!is_setup_done)
+  {
+    dnsServer.processNextRequest();
+    delay(10);
+    if (valid_ssid_received && valid_password_received)
+    {
+      Serial.println("Attempting WiFi Connection!");
+      WiFiStationSetup(ssid, password);
+    }
+  }
+
+  Serial.println("All Done!");
+
+  //Your Additional Setup code
+}
+
+void loop() {
+  //Your Loop code
+}
