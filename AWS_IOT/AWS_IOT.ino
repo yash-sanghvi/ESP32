503a9f4477bb9b1d80abcf38f088336e1fdc497d
Yash Sanghvi <50194122+sanghvi-yash@users.noreply.github.com>
2021-10-20 23:49:41 +0530

    Added detailed comments to the AWS_IOT sketch

--- AWS_IOT/AWS_IOT.ino
+++ AWS_IOT/AWS_IOT.ino
@@ -1,9 +1,9 @@
 #include "config.h"
 
 #include <WiFiClientSecure.h>
-#include <MQTTClient.h>
+#include <MQTTClient.h> //MQTT Library Source: https://github.com/256dpi/arduino-mqtt
 
-#include <ArduinoJson.h>
+#include <ArduinoJson.h> //ArduinoJson Library Source: https://github.com/bblanchon/ArduinoJson
 #include "WiFi.h"
 
 // MQTT topics for the device
@@ -11,35 +11,39 @@
 #define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
 
 WiFiClientSecure wifi_client = WiFiClientSecure();
-MQTTClient mqtt_client = MQTTClient(256);
+MQTTClient mqtt_client = MQTTClient(256); //256 indicates the maximum size for packets being published and received.
 
 uint32_t t1;
 
 void connectAWS()
 {
-  WiFi.mode(WIFI_STA);
+  //Begin WiFi in station mode
+  WiFi.mode(WIFI_STA); 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   Serial.println("Connecting to Wi-Fi");
 
+  //Wait for WiFi connection
   while (WiFi.status() != WL_CONNECTED){
     delay(500);
     Serial.print(".");
   }
+  Serial.println();
 
   // Configure wifi_client with the correct certificates and keys
   wifi_client.setCACert(AWS_CERT_CA);
   wifi_client.setCertificate(AWS_CERT_CRT);
   wifi_client.setPrivateKey(AWS_CERT_PRIVATE);
 
-  // connect to AWS IOT Broker. 8883 is the port used for MQTT
+  //Connect to AWS IOT Broker. 8883 is the port used for MQTT
   mqtt_client.begin(AWS_IOT_ENDPOINT, 8883, wifi_client);
 
-  // Action to take on incoming messages
+  //Set action to be taken on incoming messages
   mqtt_client.onMessage(incomingMessageHandler);
 
   Serial.print("Connecting to AWS IOT");
 
+  //Wait for connection to AWS IoT
   while (!mqtt_client.connect(THINGNAME)) {
     Serial.print(".");
     delay(100);
@@ -51,7 +55,7 @@ void connectAWS()
     return;
   }
 
-  // Subscribe to a topic
+  //Subscribe to a topic
   mqtt_client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
 
   Serial.println("AWS IoT Connected!");
@@ -59,12 +63,15 @@ void connectAWS()
 
 void publishMessage()
 {
+  //Create a JSON document of size 200 bytes, and populate it
+  //See https://arduinojson.org/
   StaticJsonDocument<200> doc;
   doc["elapsed_time"] = millis() - t1;
   doc["value"] = random(1000);
   char jsonBuffer[512];
   serializeJson(doc, jsonBuffer); // print to mqtt_client
 
+  //Publish to the topic
   mqtt_client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
   Serial.println("Sent a message");
 }
